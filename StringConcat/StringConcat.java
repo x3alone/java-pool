d2431e5caa26bb70beb429356aa2fda76a5fcd2f
otmane chouari <oottmmaann3@gmail.com>
2025-08-11 21:54:11 +0100

    concat string

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,11 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null){
+            s1 = "";
+        }
+        if (s2 == null){
+            s2 = "";
+        }
         return s1 + s2;
         }
 }
