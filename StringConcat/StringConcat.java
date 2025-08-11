64d002c7f458faf4380cc9854ec1d6b908f26166
otmane chouari <oottmmaann3@gmail.com>
2025-08-11 21:58:56 +0100

    concat string

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,13 +1,13 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s2 == null && s1 == null){
+            return null;
+        }
         if (s1 == null){
             s1 = "";
         }
         if (s2 == null){
             s2 = "";
-        }
-        if (s2 == null && s1 == null){
-            return null;
         }
         return s1 + s2;
         }
