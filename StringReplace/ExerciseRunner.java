27e22e209d78d64f73f0304bf1db2fe9b3768261
otmane chouari <oottmmaann3@gmail.com>
2025-08-11 22:24:35 +0100

    replacement

---
+++ StringReplace/ExerciseRunner.java
@@ -0,0 +1,7 @@
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        System.out.println(StringReplace.replace("javatpoint is a very good website", 'a', 'e'));
+        System.out.println(StringReplace.replace("my name is khan my name is java", "is","was"));
+        System.out.println(StringReplace.replace("hey i'am java", "I'am","was"));
+    }
+}
