dc6ea680e655080a9afe5b2651cae3b4ec69ea32
x3alone <otmanchouarix3@gmail.com>
2025-08-12 00:00:51 +0100

    libya

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,19 +1,21 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
-           if (s == null) return false;
+        if (s == null) return false;
 
-        int len = s.length();
+        String str = s.toLowerCase();
+
+        int len = str.length();
         if (len == 0) return true; 
 
         int mid = len / 2;
 
-        String firstHalf = s.substring(0, mid);
+        String firstHalf = str.substring(0, mid);
         String secondHalf;
 
         if (len % 2 == 0) {
-            secondHalf = s.substring(mid);
+            secondHalf = str.substring(mid);
         } else {
-            secondHalf = s.substring(mid + 1);
+            secondHalf = str.substring(mid + 1);
         }
         String reversedSecond = new StringBuilder(secondHalf).reverse().toString();
         return firstHalf.equals(reversedSecond);
