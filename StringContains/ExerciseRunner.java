e0826b40add18b52b1638f7f442a841281bf018f
otmane chouari <oottmmaann3@gmail.com>
2025-08-11 22:02:57 +0100

    contains

---
+++ StringContains/ExerciseRunner.java
@@ -0,0 +1,7 @@
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        System.out.println(StringContains.isStringContainedIn("Hell", "Highway to Hell"));
+        System.out.println(StringContains.isStringContainedIn("Hell", "Hello World !"));
+        System.out.println(StringContains.isStringContainedIn("Bonjour", "hello World !"));
+    }
+}
