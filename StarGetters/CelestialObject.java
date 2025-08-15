211fe6f167f1ba9c24a3ba2867accf8867e8be51
otmane chouari <oottmmaann3@gmail.com>
2025-08-15 16:54:39 +0100

    constors

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,50 @@
+public class CelestialObject {
+    private double x;
+    private double y;
+    private double z;
+    private String name;
+    
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+    public CelestialObject(String name, double x, double y, double z) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
