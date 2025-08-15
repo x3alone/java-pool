91546989c8bca7156c8584fd83722d8e696ef350
x3alone <otmanchouarix3@gmail.com>
2025-08-15 18:09:57 +0100

    metalslug

--- StarOverride/CelestialObject.java
+++ StarOverride/CelestialObject.java
@@ -33,4 +33,20 @@ public class CelestialObject {
     public double getZ() {
         return z;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
 }
