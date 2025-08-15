0043ebc46f3bd2f619a0d8e99642c6b1a30c310b
otmane chouari <otmanchouarix3@gamil.com>
2025-08-15 17:41:37 +0100

    starlink

--- StarStatic/CelestialObject.java
+++ StarStatic/CelestialObject.java
@@ -1,21 +1,23 @@
-class CelestialObjec {
+public class CelestialObject {
     private double x;
     private double y;
     private double z;
     private String name;
-    
+
     public CelestialObject() {
         this.x = 0.0;
         this.y = 0.0;
         this.z = 0.0;
         this.name = "Soleil";
     }
+
     public CelestialObject(String name, double x, double y, double z) {
         this.name = name;
         this.x = x;
         this.y = y;
         this.z = z;
     }
+
     public double getX() {
         return x;
     }
@@ -47,17 +49,17 @@ class CelestialObjec {
     public void setName(String name) {
         this.name = name;
     }
+
     public static final double KM_IN_ONE_AU = 150000000;
 
     public static double getDistanceBetween(CelestialObject a, CelestialObject b) {
-    double dx = a.getX() - b.getX();
-    double dy = a.getY() - b.getY();
-    double dz = a.getZ() - b.getZ();
-    return Math.sqrt(dx * dx + dy * dy + dz * dz);
-}
+        double dx = a.getX() - b.getX();
+        double dy = a.getY() - b.getY();
+        double dz = a.getZ() - b.getZ();
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
 
-public static double getDistanceBetweenInKm(CelestialObject a, CelestialObject b) {
-    return getDistanceBetween(a, b) * KM_IN_ONE_AU;
-}
-
+    public static double getDistanceBetweenInKm(CelestialObject a, CelestialObject b) {
+        return getDistanceBetween(a, b) * KM_IN_ONE_AU;
+    }
 }
