832f287e7abeb49f4342e8ca35cbe92f4d28794f
x3alone <otmanchouarix3@gmail.com>
2025-08-15 18:18:13 +0100

    metalslug

--- StarOverride/CelestialObject.java
+++ StarOverride/CelestialObject.java
@@ -1,52 +1,71 @@
-public class CelestialObject {
-    private String name;
-    private double x;
-    private double y;
-    private double z;
-
-    public CelestialObject() {
-        this.name = "Soleil";
+public  class CelestialObject{
+     double x, y, z;
+     String name;
+    public  static double KM_IN_ONE_AU =  150000000; 
+    public static  double getDistanceBetween(CelestialObject obj1, CelestialObject obj2){
+        double x = obj1.getX() - obj2.getX();
+        double y = obj1.getY() - obj2.getY();
+        double z = obj1.getZ() - obj2.getZ();
+        Double distance = Math.sqrt(x*x+y*y+z*z);
+        return distance;
+    }
+    public static double getDistanceBetweenInKm(CelestialObject obj1, CelestialObject obj2){
+        double distance =  CelestialObject.getDistanceBetween(obj1, obj2);
+        return distance * CelestialObject.KM_IN_ONE_AU;
+    }
+     public CelestialObject(){
         this.x = 0.0;
-        this.y = 0.0;
-        this.z = 0.0;
+        this.y =0.0;
+        this.z =0.0;
+        this.name = "Soleil";
     }
 
-    public CelestialObject(String name, double x, double y, double z) {
-        this.name = name;
+    public CelestialObject(String name, double x, double  y, double z){
         this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public double getZ() {
-        return z;
-    }
-
-    public void setName(String name) {
+        this.y =y;
+        this.z =z;
         this.name = name;
     }
-
-    public void setX(double x) {
+    public double  getX(){
+        return this.x;
+    }
+     public double  getY(){
+        return this.y;
+    }
+    public double  getZ(){
+        return this.z;
+    }
+    public String  getName(){
+        return this.name;
+    }
+    public void  setX(double x){
         this.x = x;
     }
-
-    public void setY(double y) {
+    public void  setY(double y){
         this.y = y;
     }
-
-    public void setZ(double z) {
+    public void  setZ(double z){
         this.z = z;
+    }
+    public void  setName(String name){
+        this.name = name;
+    }
+
+    public  String toString(){
+        String res = String.format("%s is positioned at (%.3f, %.3f, %.3f)", this.name, x,y,z);
+        return res;
+    }
+    public boolean equals(CelestialObject obj){
+        if (this == null || obj == null) return false;
+
+        if (getClass() != obj.getClass()) return false;
+
+        if (this.name == obj.name && this.x == obj.x && this.y == obj.y && this.z == obj.z){
+            return  true;
+        }
+        return false;
+    }
+    public int hashCode(){
+        return java.util.Objects.hash(name,x,y,z);
     }
 }
--- StarOverride/Star.java
+++ StarOverride/Star.java
@@ -1,39 +1,36 @@
-public class Star extends CelestialObject {
-    private double magnitude;
+java Star.java
+public  class Star extends CelestialObject{
+    private double magnitude ;
 
-    public Star() {
-        super("Soleil", 0.0, 0.0, 0.0);
-        this.magnitude = 0.0;
+    public Star(){
+        super();
     }
-
-    public Star(String name, double x, double y, double z, double magnitude) {
-        super(name, x, y, z);
+    public Star(String name, double x, double  y, double z, double magnitude ){
+        super(name,x,y,z);
         this.magnitude = magnitude;
     }
-
-    public double getMagnitude() {
+    public double  getMagnitude(){
         return magnitude;
     }
-
-    public void setMagnitude(double magnitude) {
+    public void setMagnitude(double magnitude){
         this.magnitude = magnitude;
     }
 
-    @Override
-    public int hashCode() {
-        return Double.valueOf(magnitude).hashCode();
+    public  String toString(){
+        String res = String.format("%s shines at the %.3f magnitude", this.name, magnitude);
+        return res;
     }
+    public boolean equals(Star obj){
+        if (this == null || obj == null) return false;
+        
+        if (getClass() != obj.getClass()) return false;
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (!(obj instanceof Star)) return false;
-        Star other = (Star) obj;
-        return Double.compare(this.magnitude, other.magnitude) == 0;
+        if (this.name == obj.name && this.x == obj.x && this.y == obj.y && this.z == obj.z && this.magnitude == obj.magnitude){
+            return  true;
+        }
+        return false;
     }
-
-    @Override
-    public String toString() {
-        return String.format("%s shines at the %.3f magnitude", getName(), magnitude);
+     public int hashCode(){
+        return java.util.Objects.hash(name,x,y,z,magnitude);
     }
 }
