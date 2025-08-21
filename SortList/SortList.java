3cd11d2eae69fce115a733b7de9062f808a84e40
x3alone <otmanchouarix3@gmail.com>
2025-08-21 12:39:41 +0100

    sorts

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -5,12 +5,18 @@ import java.util.Collections;
 public class SortList {
 
     public static List<Integer> sort(List<Integer> list) {
+        if (list == null){
+            return null;
+        }
         List<Integer> sortedList = new ArrayList<>(list); 
         Collections.sort(sortedList);
         return sortedList;
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
+        if (list == null){
+            return null;
+        }
         List<Integer> sortedList = new ArrayList<>(list); 
         Collections.sort(sortedList, Collections.reverseOrder());
         return sortedList;
