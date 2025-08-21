284183a72f1cc95c319b50237acf20c0c37435fb
x3alone <otmanchouarix3@gmail.com>
2025-08-21 12:52:24 +0100

    mapping thru

---
+++ MapInventory/MapInventory.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MapInventory {
+
+    public static int getProductPrice(Map<String, Integer> inventory, String productId) {
+        return inventory.getOrDefault(productId, -1);
+    }
+
+    public static List<String> getProductIdsByPrice(Map<String, Integer> inventory, int price) {
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            if (entry.getValue() == price) {
+                result.add(entry.getKey());
+            }
+        }
+        return result;
+    }
+}
