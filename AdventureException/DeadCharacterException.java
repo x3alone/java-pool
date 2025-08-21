2742ff04ccdc74d40ec97e8d10914e36e0acd07c
x3alone <otmanchouarix3@gmail.com>
2025-08-21 11:26:09 +0100

    when u guys were u younger u  loved mr flapjack

---
+++ AdventureException/DeadCharacterException.java
@@ -0,0 +1,24 @@
+
+public class DeadCharacterException extends Exception {
+
+    private final Character character;
+
+    public DeadCharacterException(Character character) {
+        this.character = character;
+    }
+
+    @Override
+    public String getMessage() {
+        String type;
+        if (character instanceof Sorcerer) {
+            type = "sorcerer";
+        } else if (character instanceof Templar) {
+            type = "templar";
+        } else if (character instanceof Monster) {
+            type = "monster";
+        } else {
+            type = "character";
+        }
+        return "The " + type + " " + character.getName() + " is dead.";
+    }
+}
