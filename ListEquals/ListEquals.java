0aec6fcc611887a3f917bfe8a6777ffd8eeb575b
x3alone <otmanchouarix3@gmail.com>
2025-08-21 12:03:01 +0100

    listequals

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,11 +2,11 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
-        if (list1 == null ||list2 == null){
-            return false;
-        }
-        if (list1 == null &&list2 == null){
+        if (list1 == null && list2 == null){
             return true;
+        }
+        if (list1 == null || list2 == null){
+            return false;
         }
         return list1.equals(list2);
     }
