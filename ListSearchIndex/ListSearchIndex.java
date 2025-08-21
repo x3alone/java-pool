605d015a7a86b20fa71e4b42c959c4d1c7c3216f
x3alone <otmanchouarix3@gmail.com>
2025-08-21 12:34:20 +0100

    taliban

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -3,10 +3,26 @@ import java.util.List;
 
 public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
-        return list.lastIndexOf(value);
+        if (list == null){
+            return null;
+        }
+        int edx = list.lastIndexOf(value);
+        if (edx >= 0){
+        return edx;
+        }else {
+            return null;
+        }
     }
     public static Integer findFirstIndex(List<Integer> list, Integer value) {
-        return list.indexOf(value);
+        if (list == null){
+            return null;
+        }
+        int edx = list.indexOf(value);
+        if (edx >= 0){
+        return edx;
+        }else {
+            return null;
+        }
     }
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
         List<Integer> indexes = new ArrayList<>();
