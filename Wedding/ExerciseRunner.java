0c2d0e9cd49424fc5770ecea5011c0051896ade1
otmane chouari <otmanchouarix3@gmail.com>
2025-08-23 11:36:43 +0100

    me nd breeze prolly around  40 yo or 39 hehe so we can have clem

--- Wedding/ExerciseRunner.java
+++ Wedding/ExerciseRunner.java
@@ -3,6 +3,6 @@ import java.util.Set;
 public class ExerciseRunner {
 
     public static void main(String[] args) {
-        System.out.println(Wedding.createCouple(Set.of("Pikachu", "Dracaufeu", "Tortank"), Set.of("Legolas", "Aragorn", "Gimli")));
+        System.out.println(Wedding.createCouple(Set.of("breez3", "clem ", "hib3rnation"), Set.of("brz", "CLE", "daughter")));
     }
 }
