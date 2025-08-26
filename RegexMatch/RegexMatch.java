250ae7c3f9a17666e89a55b4715db6cb9e65d1d8
otmane chouari <otmanchouarix3@gmail.com>
2025-08-26 10:42:42 +0100

    fam is evthang

---
+++ RegexMatch/RegexMatch.java
@@ -0,0 +1,13 @@
+public class RegexMatch {
+    public static boolean containsOnlyAlpha(String s) {
+        return s.matches("^[a-zA-Z]+$");
+    }
+    
+    public static boolean startWithLetterAndEndWithNumber(String s) {
+        return s.matches("^[a-zA-Z].*[0-9]$");
+    }
+    
+    public static boolean containsAtLeast3SuccessiveA(String s) {
+        return s.matches(".*a{3,}.*");
+    }
+}
